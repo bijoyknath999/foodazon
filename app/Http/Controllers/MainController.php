7f0e6b197ad6c27fcd2b912ec81bfc96aaa1dbe7
bijoyknath999 <bijoykumarnath999@gmail.com>
2021-05-19 22:26:06 +0600

    make it better

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -135,9 +135,16 @@ class MainController extends Controller
 
 
     function dashboard(){
-        $matchThese = ['orders_status' => 0, 'orders_type' => 1];
-                $orders = Orders::where($matchThese)->get();
-                return $orders;         
+        $totalNewUsers = Users::where('verified',0)->count();
+        $totalUsers = Users::where('verified',1)->count();
+        $totalNewOrders = Orders::where('orders_status',0)->count();
+        $totalOrders = Orders::where('orders_status',1)->count();
+        $count = ["totalnewusers" => $totalNewUsers, 
+                "totalusers" => $totalUsers, 
+                "totalneworders" => $totalNewOrders, 
+                "totalorders" => $totalOrders];
+
+        return view('admin.dashboard', ['count'=>$count]);            
     }
 
     function addfood(){
