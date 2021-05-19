21308e0f49206165bc4bfe5de9fe640b47c29d95
bijoyknath999 <bijoykumarnath999@gmail.com>
2021-05-19 14:48:13 +0600

    make it better

--- resources/views/layouts/usertopnav.blade.php
+++ resources/views/layouts/usertopnav.blade.php
@@ -24,7 +24,7 @@
     <!-- Logo -->
     <div class="logo-sn ms-d-block-lg">
       <a class="pl-0 ml-0 text-center" href="index.html">
-        <img src="{{asset('images')}}/foodazon-logo.png" alt="logo">
+        <img src="{{asset('images')}}/foodazon-logo-216-116.png" alt="logo">
       </a>
     </div>
     <!-- Navigation -->
@@ -83,7 +83,7 @@
         <span class="ms-toggler-bar bg-primary"></span>
       </div>
       <div class="logo-sn logo-sm ms-d-block-sm">
-        <a class="pl-0 ml-0 text-center navbar-brand mr-0" href="/"><img src="{{asset('images')}}/foodazon-logo.png" alt="logo"> </a>
+        <a class="pl-0 ml-0 text-center navbar-brand mr-0" href="/"><img src="{{asset('images')}}/foodazon-logo-84-84.png" alt="logo"> </a>
       </div>
       <ul class="ms-nav-list ms-inline mb-0" id="ms-nav-options">
 
