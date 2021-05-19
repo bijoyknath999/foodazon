86757a9bd88688e96e939924f439b2d4a7301e94
bijoyknath999 <bijoykumarnath999@gmail.com>
2021-05-19 20:48:05 +0600

    make it better

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -135,7 +135,7 @@ class MainController extends Controller
 
 
     function dashboard(){
-        $totalNewUsers = Users::where('verified',0)->sum('id');
+        $totalNewUsers = Users::sum('id')->where('verified',0);
         $totalUsers = Users::where('verified',1)->sum('id');
         $totalNewOrders = Orders::where('orders_status',0)->sum('id');
         $totalOrders = Orders::where('orders_status',1)->sum('id');
