6268f774310f5da1f9d57ce005334f622eceb392
bijoyknath999 <bijoykumarnath999@gmail.com>
2021-05-19 20:36:01 +0600

    make it better

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -144,7 +144,7 @@ class MainController extends Controller
                 "totalneworders" => $totalNewOrders, 
                 "totalorders" => $totalOrders];
 
-        return $count['0']->totalnewusers;
+        return $count['totalnewusers'];
         // return view('admin.dashboard', ['count'=>$count]);            
     }
 
